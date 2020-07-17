55955fb066704b43684f92e1c1adeaa389a8f28d
vodenkaj <67589938+vodenkaj@users.noreply.github.com>
2020-07-17 07:57:00 +0100

    Fixed link

--- src/loaders/mongoose.js
+++ src/loaders/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
+const config = require('../config');
 
 module.exports = () => {
-	mongoose.connect('mongodb+srv://vodenkaj:Sufix558@random-colors.trjbx.azure.mongodb.net/', {useNewUrlParser: true, useUnifiedTopology: true});
+	mongoose.connect('mongodb+srv://vodenkaj:config.MONGO_PASS@random-colors.trjbx.azure.mongodb.net/', {useNewUrlParser: true, useUnifiedTopology: true});
 }
